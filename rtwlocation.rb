aef4fdc7cd4a6d688f104cc42a7d797cbcf7819f
Arfon Smith <arfon@github.com>
2015-05-04 12:51:56 -0500

    Updating map.

--- rtwlocation.rb
+++ rtwlocation.rb
@@ -14,10 +14,10 @@ end
 post '/sms-receive' do
   lat_lng_msg = params[:Body]
   lat, lng, msg = lat_lng_msg.split(',').map(&:strip)
-  
+
   current_location = latest_geom_position_from(lat,lng, msg)
   update_gist_location(current_location)
-  
+
   twiml = Twilio::TwiML::Response.new do |r|
     r.Message "OK, I've got you at https://maps.google.com/maps/preview?q=#{lat},#{lng}"
   end
@@ -26,17 +26,17 @@ end
 
 def update_gist_location(position)
   client = Octokit::Client.new(:access_token => ENV['GH_TOKEN'])
-  old_geo = JSON.load(File.open('lowe2014.geojson', 'r').read)
+  old_geo = JSON.load(File.open('lowe2015.geojson', 'r').read)
   old_geo["features"] << position
-  
+
   client.edit_gist(ENV['GIST_ID'], {
-    :files => {"lowe2014.geojson" => {"content" => JSON.pretty_generate(old_geo)}}
+    :files => {"lowe2015.geojson" => {"content" => JSON.pretty_generate(old_geo)}}
   })
 end
 
 def latest_geom_position_from(lat,lng, msg = nil)
   position = Hash.new
-  
+
   position["type"] = "Feature"
   position["properties"] = {
     "marker-symbol" => "star",
@@ -51,9 +51,9 @@ def latest_geom_position_from(lat,lng, msg = nil)
       lat.to_f
     ]
   }
-  
+
   # Set the message property if there is one
   position["properties"]["Message"] = msg if msg
-  
+
   return position
 end
